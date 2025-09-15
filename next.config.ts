bec53f05172fe5bf102b9e6019cc3018545a3649
Jose Starsky Naya Lara <starsky@Joses-MBP.home>
2025-09-15 11:53:11 +0200

    wip

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   devIndicators: false,
+  output: "standalone",
   images: {
     domains: ["i.annih.us"],
     remotePatterns: [
